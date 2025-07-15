7b127aa4a3bfd57175f65e4381911f8a3f89ab1b
0r4nggg <o2masashi@ybb.ne.jp>
2025-07-15 21:05:50 +0900

    add node.js version specification

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB接続
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
+ .then(() => console.log('MongoDB connected'))
+ .catch(err => console.error('MongoDB connection error:', err));
 
 // スキーマ定義
 const UserSchema = new mongoose.Schema({
